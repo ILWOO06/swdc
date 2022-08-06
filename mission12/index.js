586f3728f4e17246f9fa8b03353f2de251d62834
ILWOO06 <ilwoo0504@naver.com>
2022-08-06 20:00:55 +0900

    8/6

--- mission12/index.js
+++ mission12/index.js
@@ -1,2 +1,13 @@
-var now=new Date();
-console.log("현재 : ",now.toLocaleString());
+function add(length){
+    if(length<10){
+        return `0${length}`
+    }else{
+    return `${length}`
+    }
+}
+var now = new Date();
+var hours = now.getHours();
+var minutes = now.getMinutes();
+var seconds = now.getSeconds();
+
+console.log(`${add(hours)}:${add(minutes)}:${add(seconds)}`)
